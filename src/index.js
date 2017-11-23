5e957c9a7dfc9c337a386a361fe7aa9f58d3237c
Kunal Ojha <kunalojha@Kunals-MacBook-Pro.local>
2017-11-23 01:09:50 -0500

    created first component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar'
+
 const API_KEY = 'AIzaSyCglWaCRiL8EJin1HYJ-9a_g7yEYy3IDj0';
 
 const App = () => {
-    return <div>Hi!</div>;
+return (
+<div>
+    <SearchBar/>
+</div>);
 }
 
 //Take this component's generated HTML and put it on the page (in the DOM)
